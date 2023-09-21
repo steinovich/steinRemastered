b768cbf60d59fd67e19c7bdbafdcc78f7063909f
Parnevich <k.p.electr@gmail.com>
2023-09-21 16:27:15 +0300

    21.09.23

--- src/BaseInterface.java
+++ src/BaseInterface.java
@@ -1,9 +1,6 @@
 import java.io.IOException;
 
 public interface BaseInterface {
-
-    //   boolean createOutputFile() throws IOException;
-
     boolean emptyBaseFile();
 
     void addAccount(String name, int pass) throws IOException, ClassNotFoundException;
@@ -13,12 +10,5 @@ public interface BaseInterface {
     int getPass(String name) throws IOException, ClassNotFoundException;
 
     boolean checkExistName(String name) throws IOException, ClassNotFoundException;
-
-//    String getAccount(String part, String name) throws IOException;
-//    boolean deleteAccount(String part, String name) throws IOException;
-
 }
-//09.07.23
-//23.08.23
-//baseAccount2
 
